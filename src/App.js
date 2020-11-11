46bf037261f12dcff6aafc728391dbff64bd3479
vivianachoque <vivianaachoque@gmail.com>
2020-11-11 12:27:40 -0300

    borro componente Navigation y creo json para simular datos extraidos por un servidor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Navigation from "../src/components/Navigation"
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Navigation/>
+        <nav className="navbar navbar-dark bg-dark">
+          <a href="" className="text-white">
+            Tareas
+          </a>
+        </nav>
+
         <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
